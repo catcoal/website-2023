634c02ee14cb3e4182c944aea7cbc318a950572d
Lemming <l.interlaken.lem@gmail.com>
2023-11-17 15:33:31 +0800

    refactor:用 @vercel/analytics 替换 vue-matomo 以提升分析能力

--- plugins/matomo-plugin.client.ts
+++ plugins/matomo-plugin.client.ts
@@ -1,17 +1,20 @@
-import VueMatomo from "vue-matomo";
+// 2023/11/17号使用vercel的分析
+// 用户分析插件vue-matomo（关闭）
 
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(VueMatomo, {
-    debug: true,
-    host: "https://matomo.lemming.top/",
-    siteId: 1,
-    router: nuxtApp.$router,
-    enableHeartBeatTimer: true,
-    heartBeatTimerInterval: 5,
-    enableLinkTracking: true,
-    requireConsent: false,
-    trackInitialView: true,
-    disableCookies: true,
-    requireCookieConsent: false,
-  });
-});
+// import VueMatomo from "vue-matomo";
+
+// export default defineNuxtPlugin((nuxtApp) => {
+//   nuxtApp.vueApp.use(VueMatomo, {
+//     debug: true,
+//     host: "https://matomo.lemming.top/",
+//     siteId: 1,
+//     router: nuxtApp.$router,
+//     enableHeartBeatTimer: true,
+//     heartBeatTimerInterval: 5,
+//     enableLinkTracking: true,
+//     requireConsent: false,
+//     trackInitialView: true,
+//     disableCookies: true,
+//     requireCookieConsent: false,
+//   });
+// });
